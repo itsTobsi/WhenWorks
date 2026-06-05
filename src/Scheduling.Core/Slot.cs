15dbece4a3d28b46db9c63259629d56bbb965c4e
Tobsi <61752@edu.campusvejle.dk>
2026-06-05 07:06:07 -0500

    Project setup

---
+++ src/Scheduling.Core/Slot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scheduling.Core
+{
+    internal class Slot
+    {
+    }
+}
---
+++ src/WhenWorks/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WhenWorks.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
